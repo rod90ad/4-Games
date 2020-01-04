4db82ff11301cb737bb477cd84ce88265ea6fad0
Rodrigo Aguiar Dias <rod90ad@gmail.com>
2020-01-04 18:29:58 -0300

    Corrigido stack overflow na recursividade e bug de cor no primeiro bloco

--- lib/Minesweeper/minesweeper.dart
+++ lib/Minesweeper/minesweeper.dart
@@ -6,20 +6,23 @@ import 'package:flutter/material.dart';
 class Minesweeper extends StatelessWidget {
   
   List<List<MineSweeperModel>> field = List<List<MineSweeperModel>>();
+  List<List<GlobalKey<MineSweeperFieldState>>> keys = new List<List<GlobalKey<MineSweeperFieldState>>>();
   final int numberOfRows = 18;
   final int numberOfColumns = 14;
   final int numberOfMines = 40;
+  bool isGameOver = false;
   
   @override
   Widget build(BuildContext context) {
     _generateMines();
     _calculateNumberAround();
-    _createField();
+    _setColors();
     return Container(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
       margin: EdgeInsets.only(left: 4, top: 100, right: 4, bottom: 4),
       child: GridView.count(
+        key: Key("gridView"),
         crossAxisCount: numberOfColumns,
         childAspectRatio: 1.0,
         padding: EdgeInsets.all(4),
@@ -28,19 +31,58 @@ class Minesweeper extends StatelessWidget {
     );
   }
 
-  List<Widget> _getListOfWidgets(){
-    List<Widget> list = new List<Widget>();
-    for(int i=0; i< numberOfRows; i++){
-      for(int j=0;j<numberOfColumns; j++){
-        list.add(MineSweeperField(field[i][j]));
+  void gameOver()async{
+    for(int i=0;i<numberOfRows;i++){
+      for(int j=0;j<numberOfColumns;j++){
+        if(field[i][j].bomb) await keys[i][j].currentState.onTap();
       }
     }
-    return list;
   }
 
-  void _createField(){
+  void verifyVictory(){
+    for(int i=0;i<numberOfRows;i++){
+      for(int j=0;j<numberOfColumns;j++){
+        if(!field[i][j].bomb)
+          if(!field[i][j].clicked)
+            return;
+      }
+    }
+    print("Victory!!!");
+  }
+
+  Future<void> _clickLeftTop(int r, int c) async { if(r-1>=0 && c-1>=0) await keys[r-1][c-1].currentState.onTap(); return; } //diagonal esquerda cima
+  Future<void> _clickTop(int r, int c) async { if(r-1>=0) await keys[r-1][c].currentState.onTap(); return; } //cima
+  Future<void> _clickRightTop(int r, int c) async { if(r-1>=0 && c+1<numberOfColumns) await keys[r-1][c+1].currentState.onTap(); return; } //diagonal direita cima
+  Future<void> _clickLeft(int r, int c) async { if(c-1>=0) await keys[r][c-1].currentState.onTap(); return; } //esquerda
+  Future<void> _clickRight(int r, int c) async { if(c+1<numberOfColumns) await keys[r][c+1].currentState.onTap(); return; } //direita
+  Future<void> _clickLeftBottom(int r, int c) async { if(r+1<numberOfRows && c-1>=0) await keys[r+1][c-1].currentState.onTap(); return; } //diagonal esquerda baixo
+  Future<void> _clickBottom(int r, int c) async { if(r+1<numberOfRows) await keys[r+1][c].currentState.onTap(); return; } //baixo
+  Future<void> _clickRightBottom(int r, int c) async { if(r+1<numberOfRows && c+1<numberOfColumns) await keys[r+1][c+1].currentState.onTap(); return; } //diagonal direita baixo
+
+  Future<void> clickAround(int r, int c) async {
+    await _clickLeftTop(r, c);
+    await _clickTop(r, c);
+    await _clickRightTop(r, c);
+    await _clickLeft(r, c);
+    await _clickRight(r, c);
+    await _clickLeftBottom(r, c);
+    await _clickBottom(r, c);
+    await _clickRightBottom(r, c);
+    return;
+  }
+
+  List<Widget> _getListOfWidgets(){
+    List<Widget> listOfWidgets = new List<Widget>();
+    for(int i=0; i< numberOfRows; i++){
+      for(int j=0;j<numberOfColumns; j++){
+        listOfWidgets.add(MineSweeperField(field[i][j], this, keys[i][j]));
+      }
+    }
+    return listOfWidgets;
+  }
+
+  void _setColors(){
     Color color = Colors.green[500];
-    int line = 0;
     for(int i=0; i < numberOfRows; i++){
       if(color==Colors.green[500])
         color=Colors.green[600];
@@ -63,14 +105,14 @@ class Minesweeper extends StatelessWidget {
           field[r][c].numberAround=-1;
         }else{
           int around = 0;
-          if((r-1>=0 && c-1>=0) && field[r-1][c-1].bomb) around++; //diagonal esquerda cima
+          if((r-1>=0 && c-1>=0) && field[r-1][c-1].bomb) around++;  //diagonal esquerda cima
           if((r-1>=0) && field[r-1][c].bomb) around++; //cima
           if((r-1>=0 && c+1<numberOfColumns) && field[r-1][c+1].bomb) around++; //diagonal direita cima
           if((c-1>=0) && field[r][c-1].bomb) around++; //esquerda
           if(c+1<numberOfColumns && field[r][c+1].bomb) around++; //direita
-          //if((r+1<numberOfRows && c-1>=0) && field[r-1][c-1].bomb) around++; //diagonal esquerda cima
-          //if((r+1>=0) && field[r+1][c].bomb) around++; //cima
-          //if((r+1>=0 && c+1<numberOfRows) && field[r+1][c+1].bomb) around++; //diagonal direita cima
+          if((r+1<numberOfRows && c-1>=0) && field[r+1][c-1].bomb) around++; //diagonal esquerda baixo
+          if((r+1<numberOfRows) && field[r+1][c].bomb) around++; //baixo
+          if((r+1<numberOfRows && c+1<numberOfColumns) && field[r+1][c+1].bomb) around++; //diagonal direita baixo
           field[r][c].numberAround = around;
         }
       }
@@ -80,8 +122,10 @@ class Minesweeper extends StatelessWidget {
   void _generateMines(){
     field.clear();
     for(int i=0;i<numberOfRows;i++){
+      keys.add(List<GlobalKey<MineSweeperFieldState>>());
       field.add(List<MineSweeperModel>());
       for(int j=0;j<numberOfColumns;j++){
+        keys[i].add(GlobalKey<MineSweeperFieldState>());
         field[i].add(MineSweeperModel(bomb: false, numberOfRow: i, numberOfColumn: j));
       }
     }
@@ -93,5 +137,5 @@ class Minesweeper extends StatelessWidget {
       else
         i--;
     }
-  }
+  }  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,10 @@ import 'diamond_menu.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Diamond Menu',
+      title: '4 Games',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
